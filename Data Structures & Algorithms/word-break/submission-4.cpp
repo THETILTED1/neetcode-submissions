e580e769b834bea809a817dee2742999e144db3d
Nathaniel Potter <105668379+THETILTED1@users.noreply.github.com>
2026-09-15 18:59:55 -0500

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int sz = s.size();
+
+        unordered_map<int, unordered_set<string>> wd{};
+        for (string& t : wordDict) 
+            wd[t.size()].insert(t);
+
+        vector<bool> dp(sz + 1, false);
+        dp[sz] = true;
+    
+        for (int l = sz - 1; l >= 0; l--){
+            for (const auto& pp : wd){
+                if (l + pp.first <= sz and dp[l + pp.first] and 
+                    pp.second.contains(s.substr(l, pp.first))){
+
+                    dp[l] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
