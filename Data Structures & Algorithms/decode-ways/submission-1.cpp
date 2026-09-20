072df89a29708759b37fbba56c4a1940de93f3a7
Nathaniel Potter <105668379+THETILTED1@users.noreply.github.com>
2026-09-20 14:52:29 -0500

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool getTwo(string& s, int i){
+        if (i + 2 > s.size() or i < 0 or s[i] < '1')
+            return false;
+        return (10 * (s[i] - '0') + s[i + 1] - '0') < 27;
+    }
+    bool getOne(string& s, int i){
+        if (i >= s.size() or i < 0)
+            return false;
+        return s[i] > '0';
+    }
+    int numDecodings(string s) {
+        int two = 1, one = 1;
+        for (int i = 0; i < s.size(); i++){
+            int tmp = one;
+            one = getOne(s, i) * one + getTwo(s, i - 1) * two;
+            two = tmp;
+        }
+        return one;
+    }
+};
