ae51c6a16489f99a1041c51fa5315a26ded21cef
Nathaniel Potter <105668379+THETILTED1@users.noreply.github.com>
2026-09-25 16:29:46 -0500

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 1)
+            return 0;
+        if (n == 2)
+            return max(0, prices[1] - prices[0]);
+
+        vector<pair<int, int>> dp(n);
+
+        dp[0] = {0, -prices[0]};
+        dp[1] = {max(prices[1] - prices[0], 0), max(-prices[0], -prices[1])};
+
+        for (int i = 2; i < n; i++){
+            dp[i].first = max(dp[i - 1].first, dp[i - 1].second + prices[i]);
+            dp[i].second = max(dp[i - 1].second, dp[i - 2].first - prices[i]);
+        }
+
+        return dp[n - 1].first;
+    }
+};
