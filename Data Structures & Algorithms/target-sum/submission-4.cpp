2060ec9d213f270eb3d279fff9ffe0d94fac4c62
Nathaniel Potter <105668379+THETILTED1@users.noreply.github.com>
2026-09-26 11:29:19 -0500

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > sum)
+            return 0;
+        if ((sum + target) & 1)
+            return 0;
+
+        int amount = (target + sum) / 2;
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        int best = 0;
+        for (int n : nums)
+            for (int j = amount - n; j >= 0; j--)
+                dp[j + n] += dp[j];
+        
+        return dp[amount];        
+    }
+};
